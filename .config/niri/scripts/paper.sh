75c77ddbe98ce277e066d37d6168d369be5b5000
nishiiko <nishiiko@protonmail.com>
2025-06-30 12:12:47 +0100

    modified:   .config/niri/scripts/paper.sh

--- .config/niri/scripts/paper.sh
+++ .config/niri/scripts/paper.sh
@@ -4,5 +4,5 @@ until pgrep -x swww-daemon; do
     swww-daemon --no-cache &
 done
 
-swww img $HOME/Pictures/Wallpapers/omori.png --transition-type grow --transition-duration 3
-swaybg --image $HOME/Pictures/Wallpaper/omori_blur.png
+swww img $HOME/Pictures/Wallpapers/omori.png --transition-type grow --transition-duration 3 &
+swaybg --image $HOME/Pictures/Wallpapers/omori_blur.png &
