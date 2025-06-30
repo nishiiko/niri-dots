d26b6f263df46a75143b21ce49f2b06b62cd8827
nishiiko <nishiiko@protonmail.com>
2025-06-30 23:33:52 +0100

    modified:   .config/niri/config.kdl 	modified:   .config/niri/scripts/portals.sh

--- .config/niri/scripts/portals.sh
+++ .config/niri/scripts/portals.sh
@@ -2,7 +2,7 @@
 
 if ! grep -r systemd /sbin/init; then
     sleep 1
-    killall -e xdg-desktop-portal*
+    pkill -f xdg-desktop-portal
     /usr/lib*/xdg-desktop-portal-gnome &
     /usr/lib*/xdg-desktop-portal-gtk &
 fi
