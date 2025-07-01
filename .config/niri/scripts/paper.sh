410c6e3d32bbe2c17cb8a1576254989528371ad8
nishiiko <nishiiko@protonmail.com>
2025-07-01 18:03:07 +0100

    nvm i didnt like alt

--- .config/niri/scripts/paper.sh
+++ .config/niri/scripts/paper.sh
@@ -2,7 +2,8 @@
 
 until pgrep -x swww-daemon; do
     swww-daemon --no-cache &
+    sleep 0.2s
 done
 
-swww img $HOME/Pictures/Wallpapers/omori.png --transition-type grow --transition-duration 3 &
 swaybg --image $HOME/Pictures/Wallpapers/omori_blur.png &
+swww img $HOME/Pictures/Wallpapers/omori.png --transition-type grow --transition-duration 3 &
