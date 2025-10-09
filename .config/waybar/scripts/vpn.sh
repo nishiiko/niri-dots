6d9bd97f6f77f5f3b8d453dfe6ef5e6a498f4fc0
nishiiko <nishiiko@protonmail.com>
2025-10-09 14:26:48 +0100

    modified:   .config/waybar/config.jsonc 	modified:   .config/waybar/scripts/vpn.sh 	.config/waybar/scripts/vpn-watch.sh

--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-ip=$(curl -s https://icanhazip.com)
 
-# only works with mullvad, adjust as needed
-mullvad status | grep "Connected" &>/dev/null \
+mullvad status | grep -e "Connected" -e "Connecting" &>/dev/null \
+&& ip=$(curl -s4 https://icanhazip.com) \
 && jq -c -n --arg ip "$ip" '{"text":$ip,"class":"connected","percentage":100}' \
 || echo '{"text":"VPN Disconnected","class":"disconnected","percentage":0}'
