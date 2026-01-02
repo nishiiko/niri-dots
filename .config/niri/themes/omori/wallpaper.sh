c736ccac5fa72070e26701342275520593738dde
nishiiko <towayami@gmail.com>
2026-01-02 11:35:27 +0000

    modified:   .config/niri/config.kdl.d/layerrules.kdl 	modified:   .config/niri/themes/omori/wallpaper.sh 	modified:   .config/niri/themes/tanger/wallpaper.sh

--- .config/niri/themes/omori/wallpaper.sh
+++ .config/niri/themes/omori/wallpaper.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 swww-daemon --no-cache & disown
+swww-daemon --no-cache --namespace blur & disown
 sleep 0.2s
-swaybg -i $HOME/.config/niri/themes/omori/backdrop.png &
 swww img $HOME/.config/niri/themes/omori/omori.png --transition-step 255 &
+swww img --namespace blur $HOME/.config/niri/themes/omori/backdrop.png --transition-step 255 &
--- .config/niri/themes/tanger/wallpaper.sh
+++ .config/niri/themes/tanger/wallpaper.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 swww-daemon --no-cache & disown
+swww-daemon --no-cache --namespace blur & disown
 sleep 0.2s
-swaybg -i $HOME/.config/niri/themes/tanger/backdrop.png &
 swww img $HOME/.config/niri/themes/tanger/tanger.png --transition-step 255 &
+swww img --namespace blur $HOME/.config/niri/themes/tanger/backdrop.png --transition-step 255 &
