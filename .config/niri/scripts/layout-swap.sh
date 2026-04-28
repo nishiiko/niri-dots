1d542855cec2a538700382c80657823f9dc7d456
nishiiko <nishi@nishi.nishi>
2026-04-28 12:21:05 +0100

    new file:   .config/niri/extras/layouts/English (UK) 	new file:   .config/niri/extras/layouts/English (UK, Colemak-DH) 	new file:   .config/niri/scripts/layout-swap.sh

---
+++ .config/niri/scripts/layout-swap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+niri msg action switch-layout "next"
+layout=$(niri msg --json keyboard-layouts | jq -r '.names.[.current_idx]')
+ln -sf $HOME/.config/niri/extras/layouts/"$layout" $HOME/.config/niri/config.kdl.d/keybinds.kdl
+notify-send "Switched Layout" "Current layout: $layout"
