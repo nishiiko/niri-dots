873e7a595687015322c80b519b261a7ce479d89e
nishiiko <nishi@nishi.nishi>
2026-04-28 12:44:49 +0100

    modified:   .config/niri/scripts/layout-swap.sh 	modified:   .config/swaync/config.json

--- .config/niri/scripts/layout-swap.sh
+++ .config/niri/scripts/layout-swap.sh
@@ -2,4 +2,4 @@
 niri msg action switch-layout "next"
 layout=$(niri msg --json keyboard-layouts | jq -r '.names.[.current_idx]')
 ln -sf $HOME/.config/niri/extras/layouts/"$layout" $HOME/.config/niri/config.kdl.d/keybinds.kdl
-notify-send "Switched Layout" "Current layout: $layout"
+notify-send "Switched Layout" "Current layout: $layout" -ae niri-script
