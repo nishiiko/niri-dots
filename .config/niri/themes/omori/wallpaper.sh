184552b0a5f14a6547a05dea19cfc32f8f9fc801
nishiiko <nishi@nishi.nishi>
2026-05-07 09:44:45 +0100

    modified:   .config/niri/config.kdl.d/layerrules.kdl 	modified:   .config/niri/themes/omori/wallpaper.sh 	modified:   .config/niri/themes/tanger/wallpaper.sh

--- .config/niri/themes/omori/wallpaper.sh
+++ .config/niri/themes/omori/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-swww-daemon --no-cache & disown
-swww-daemon --no-cache --namespace blur & disown
+awww-daemon --no-cache & disown
+awww-daemon --no-cache --namespace blur & disown
 sleep 0.2s
-swww img $HOME/.config/niri/themes/omori/omori.png --transition-step 255 &
-swww img --namespace blur $HOME/.config/niri/themes/omori/backdrop.png --transition-step 255 &
+awww img $HOME/.config/niri/themes/omori/omori.png --transition-step 255 &
+awww img --namespace blur $HOME/.config/niri/themes/omori/backdrop.png --transition-step 255 &
--- .config/niri/themes/tanger/wallpaper.sh
+++ .config/niri/themes/tanger/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-swww-daemon --no-cache & disown
-swww-daemon --no-cache --namespace blur & disown
+awww-daemon --no-cache & disown
+awww-daemon --no-cache --namespace blur & disown
 sleep 0.2s
-swww img $HOME/.config/niri/themes/tanger/tanger.png --transition-step 255 &
-swww img --namespace blur $HOME/.config/niri/themes/tanger/backdrop.png --transition-step 255 &
+awww img $HOME/.config/niri/themes/tanger/tanger.png --transition-step 255 &
+awww img --namespace blur $HOME/.config/niri/themes/tanger/backdrop.png --transition-step 255 &
