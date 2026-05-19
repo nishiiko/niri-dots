f757dcd6a26dcc26c822f418873825f0250ad9a7
nishiiko <nishi@nishi.nishi>
2026-05-19 06:24:59 +0100

    modified:   .config/niri/scripts/inhibit.sh

--- .config/niri/scripts/inhibit.sh
+++ .config/niri/scripts/inhibit.sh
@@ -1,21 +1,54 @@
 #!/usr/bin/env bash
 
-casts=0
+casts=()
+
+screencastCheck () {
+	if [ "$screencastIsActive" == false ]; then
+		echo "false"
+		return
+	fi
+
+	if [ "$screencastTarget" = "{\"Nothing\":{}}" ]; then
+		echo "false"
+		return
+	fi
+
+	if [[ ${casts[@]} =~ $screencastStream ]]; then
+		echo "null"
+		return
+	fi
+
+	echo "true"
+	return
+}
 
 while read -r line; do
 
-	if [[ "$line" == true ]]; then
-		((casts++))
-	else
-		((casts--))
-	fi
+	screencastIsActive=$(echo $line | jq -rc '.CastStartedOrChanged.cast.is_active')
+	screencastTarget=$(echo $line | jq -rc '.CastStartedOrChanged.cast.target')
+	screencastStream=$(echo $line | jq -rc ' .CastStartedOrChanged.cast.stream_id')
+	screencastStatus=$(screencastCheck)
 
-	if (($casts > 0)); then
+	printf "\n$screencastIsActive"
+	printf "\n$screencastTarget"
+	printf "\n$screencastStream"
+	printf "\n$screencastStatus"
+
+	if [ "$screencastStatus" == true ]; then
+		printf "\nAdding $screencastStream"
+		casts+=$screencastStream
+	elif [ "$screencastStatus" == "false" ]; then
+		casts=(${casts[@]/$screencastStream})
+	fi
+	printf "\nCasts: ${casts[@]}"
+
+	if (( ${#casts[@]} != 0 )); then
+		printf "\n"
 		swaync-client --inhibitor-add "xdg-desktop-portal-gnome"
-		echo 'WATCH OUT YOURE SCREENSHARING'
-		continue
+		printf "\nWATCH OUT YOURE SCREENSHARING"
+	else
+		printf "\nRemoving inhibitor\n"
+		swaync-client --inhibitor-remove "xdg-desktop-portal-gnome"
 	fi
-	echo 'hi'
-	swaync-client --inhibitor-remove "xdg-desktop-portal-gnome"
 
-done < <(niri msg --json event-stream | jq -rc --unbuffered 'select(.CastStartedOrChanged) | .CastStartedOrChanged.cast.is_active')
+done < <(niri msg --json event-stream | jq -rc --unbuffered 'select(.CastStartedOrChanged)')
