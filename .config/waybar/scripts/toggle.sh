5bd79044a19ae9151fe255350c2e8b854d683f47
nishiiko <nishi@nishi.nishi>
2026-05-20 13:38:55 +0100

    modified:   .config/waybar/scripts/toggle.sh 	modified:   .config/waybar/scripts/vpn.sh

--- .config/waybar/scripts/toggle.sh
+++ .config/waybar/scripts/toggle.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-read -r state country city <<< $(mullvad status --json | jq -r '.state, .details.location.country, .details.location.city' | tr '\n' ' ')
+read -r state country city <<< $(mullvad status --json | jq -rj '.state, " ", .details.location.country, " ", .details.location.city')
 
 if [ "$state" = "connected" ]; then
   mullvad disconnect
-  notify-send "VPN Toggle" "Disconnecting from $country, $city..." -ae niri-script
+  notify-send "VPN State" "Disconnected from $country, $city" -ae niri-script
 else
   mullvad connect
   until mullvad status --json | grep connecting; do
     :
   done
-    read -r country city <<< $(mullvad status --json | jq -r '.details.location | .country, .city' | tr '\n' ' ')
-  notify-send "VPN Toggle" "Connecting to $country, $city..." -ae niri-script
-
+    read -r country city <<< $(mullvad status --json | jq -rj '.details.location | .country, " ", .city' | tr '\n' ' ')
+  notify-send "VPN State" "Connecting to $country, $city..." -ae niri-script
 fi
