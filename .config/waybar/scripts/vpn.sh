4c99d645c6c048c37598c400d2e28ec5cd2f4f0f
nishiiko <nishi@nishi.nishi>
2026-06-01 10:03:08 +0100

    modified:   .config/waybar/scripts/vpn.sh

--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -15,7 +15,6 @@ sleep 0.2s
 state=$(mullvad status --json | jq -r '.state')
 
 if [ "$state" = "disconnected" ] || [ "$state" = "disconnecting" ] ; then
-	echo '{"text":"VPN Disconnected","class":"disconnected","percentage":0}'
 	exit
 fi
 
@@ -40,5 +39,4 @@ read -r country city <<< $(mullvad status --json | jq -rj '.details.location | .
 vpn_ip=$(curl -s4 ipinfo.io | jq -r '"\(.ip) \(.country)"')
 if [[ -n "$vpn_ip" ]]; then
 	jq -c -n --arg ip "$vpn_ip" '{"text":$ip,"class":"connected","percentage":100}'
-	notify-send "VPN State" "Connected to $country, $city" -ae niri-script
 fi
